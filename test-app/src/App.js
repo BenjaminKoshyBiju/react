85f6bbbbd43f02f0186a09bf73f99f6dc1876113
BenjaminKoshyBiju <bennn455@gmail.com>
2023-09-28 23:56:49 +0530

    Added Toggle Darkmode

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 // import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TestForm from './Components/TestForm';
+import { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const darkMode=()=>{
+    if (mode==='light')
+    {
+      setMode('dark')
+    }
+    else{
+      setMode('light')
+    }
+  }
   return (
     <>
-  <Navbar title="Testing"/>
+  <Navbar title="Testing" mode={mode} darkMode={darkMode}/>
   <div className="container">  
   <TestForm heading="Text Converter"/>
   {/* <About/> */}
